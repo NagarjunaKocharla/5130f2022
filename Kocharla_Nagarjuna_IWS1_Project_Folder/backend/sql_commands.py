ddbd9300575f3940950667f7bb3930902bdb2e33
NagarjunaKocharla <113453017+NagarjunaKocharla@users.noreply.github.com>
2022-12-02 11:07:36 -0500

    Delete sql_commands.py

--- Kocharla_Nagarjuna_IWS1_Project_Folder/backend/sql_commands.py
+++
@@ -1,5 +0,0 @@
-get_transactions = f"SELECT * FROM transactions where status!='delete'"
-get_detail_coinwise = f"SELECT  symbol,type, SUM(value_usd)/100 as total_value,SUM(coins) as total_coins FROM transactions where status!='delete' GROUP BY symbol, type"
-
-
-
